24941ff5bd294b0c166456299fc076046bd9415d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-11 14:59:52 -0500

    server error fixed

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${MONGODB_PASSWORD}@deltav.mvoo1ik.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@deltav.mvoo1ik.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 500;
 
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
-
-mongoose.set('useFindAndModify', false);
